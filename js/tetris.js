0fd8f5b7abc2737d6fa014157857dde824debd66
ys <kangyoonsoon@gmail.com>
2021-09-07 14:34:11 -0400

    implements initial blocks to show it in matrix

--- js/tetris.js
+++ js/tetris.js
@@ -1,16 +1,74 @@
+// DOM
+
 const game = document.querySelector(".game > ul");
 
-console.log(game);
+// SETTING
+const GAME_ROWS = 20;
+const GAME_COLS = 10;
 
-for (let i = 0; i < 20; i++) {
+// variables
+let score = 0;
+let duration = 500;
+let downInterval;
+let tempMovingItem;
+
+const BLOCKS = {
+  tree: [
+    [
+      [2, 1],
+      [0, 1],
+      [1, 0],
+      [1, 1],
+    ],
+    [],
+    [],
+    [],
+  ],
+};
+
+const movingItem = {
+  type: "tree",
+  direction: 0,
+  top: 0,
+  left: 3,
+};
+
+init();
+
+// functions
+function init() {
+  tempMovingItem = { ...movingItem };
+
+  for (let i = 0; i < GAME_ROWS; i++) {
+    prependNewLine();
+  }
+
+  renderBlocks();
+}
+
+function prependNewLine() {
   const li = document.createElement("li");
   const ul = document.createElement("ul");
 
-  for (let j = 0; j < 10; j++) {
+  for (let j = 0; j < GAME_COLS; j++) {
     const horizon = document.createElement("li");
     ul.prepend(horizon);
   }
 
   li.prepend(ul);
   game.prepend(li);
+}
+
+function renderBlocks() {
+  const { type, direction, top, left } = tempMovingItem;
+
+  BLOCKS[type][direction].forEach((block) => {
+    const x = block[0] + left;
+    const y = block[1] + top;
+    // console.log({ game });
+    const target = game.childNodes[y].childNodes[0].childNodes[x];
+    // console.log(target);
+    target.classList.add(type);
+  });
+  console.log(BLOCKS[type][direction]);
 }
